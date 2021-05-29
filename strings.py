66e6667f5c22511163e533b965bde5b1bcb48700
Greg Schuman <gcschuman@hotmail.com>
2021-05-29 15:13:24 -0700

    Added basic concepts of strings

---
+++ strings.py
@@ -0,0 +1,11 @@
+# Print a string
+print('Hello World!')
+
+# Print a new line
+print('\n')
+
+# Print a multi-line string
+print("""This string runs multiple lines""")
+
+# String concatenation
+print("This string is " + "awesome!") 
