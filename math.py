e44b4c9de4d8b2f26a6b787d5a1cbc3ed483dd26
Greg Schuman <gcschuman@hotmail.com>
2021-05-30 10:10:31 -0700

    Added math and variables refreshers.

---
+++ math.py
@@ -0,0 +1,23 @@
+# Addition in Python
+print(1 + 1)
+
+# Subtraction in Python
+print(50 - 32)
+
+# Multplication in Python
+print(500 * 25)
+
+# Division in Python
+print(100 / 6)
+
+# PEMDAS in Python
+print (3+5*8-4/2)
+
+# Exponents in Python
+print(10**10)
+
+# Modulo in Python
+print(50 % 6)
+
+# Division in Python, but no remainder
+print(50 // 6)
---
+++ variables.py
@@ -0,0 +1,19 @@
+# Printing a quote
+quote = "60 Percent of the time, it works every time"
+print (quote)
+
+# Printing a quote in all uppercase
+print (quote.upper())
+
+# Printing a quote in lowercase
+print (quote.lower())
+
+# Printing a quote in title case
+print (quote.title())
+
+# Printing the length of a quote
+print(len(quote))
+
+# String
+name = "Greg" 
+age = 31
